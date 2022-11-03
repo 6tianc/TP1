199243c2bc213803547c65c5f968d3e965c471e9
6tianc <6tianc@gmail.com>
2022-11-03 14:27:07 -0400

    Public Repoo

---
+++ dessiner.py
@@ -0,0 +1,44 @@
+# authors: Tian Cheng Liu 20050130
+#          Flora Kang     20239294
+# November 1st 2022
+# Travail Pratique 1
+
+# codeBoot functions: setScreenMode(width, height), getScreenWidth(),
+# getScreenHeight(), setPixel(x, y, color), 
+# fillRectangle(x, y, width, height, color), getPixel(x, y), getMouse(),
+# exportScreen()
+
+def createButtons(colors, size, space, colorErase):
+    pass
+
+def findButtons(buttons, position):
+    pass
+
+def drawFloatingRectangle(originalImage, start, color):
+    pass
+
+def restoreImage(originalImage, rectangle):
+    pass
+
+def addRectangle(image, rectangle, color):
+    pass
+
+def handleNextClick(buttons):
+    pass
+
+def draw():
+    width = 180
+    height = 120
+    white = "#fff"
+    black =  "#000"
+    red = "#f00"
+    yellow = "#ff0"
+    lime = "#0f0"
+    blue =  "#00f"
+    fuchsia = "#f0f"
+    colors = [white, black, red, yellow, lime, blue, fuchsia]
+
+def testDraw():
+    assert (1 == 1)
+
+testDraw()
